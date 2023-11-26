f1d0091753f2f5b2ca7a3de66fe0635b938a092a
rkla <robertklassert@pm.me>
2023-11-26 14:01:23 +0100

    Add return based metrics and documentation

--- ccs.py
+++ ccs.py
@@ -1,6 +1,6 @@
 import shutil
 import os
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, DQN
 from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3.common.vec_env import VecFrameStack, VecTransposeImage
 from sb3_extract import (
@@ -21,6 +21,7 @@ from imitation.data.serialize import save, load_with_rewards
 def generate_dataset(
     env_name, model_name, num_episodes, max_episode_length, num_envs, seed
 ):
+    """Generate trajectory data using the given environment and model."""
     model = load_model(model_name)
     env = make_atari_env(
         env_name,
@@ -40,8 +41,10 @@ def generate_dataset(
 
 
 def load_data(model_name, val_fraction, seed):
+    """Load and split data of given model."""
     dataset = load_with_rewards(f"datasets/{model_name}")
     val_length = int(len(dataset) * val_fraction)
+    assert val_length > 0, "Validation fraction is too low!"
     train_length = len(dataset) - val_length
     train_dataset, val_dataset = data_th.random_split(
         dataset,
@@ -52,32 +55,28 @@ def load_data(model_name, val_fraction, seed):
 
 
 def load_model(model_name):
-    return PPO.load(f"agents/{model_name}", device="cpu")
+    """Load sb3 model."""
+    algo_name = model_name.split("-")[0]
+    algo_map = {"ppo": PPO, "dqn": DQN}
+    return algo_map[algo_name].load(f"agents/{model_name}", device="cpu")
 
 
-def get_preprocessed_value_estimate(model, obs):
-    pass
+def switch_player_perspective(env_name, obs):
+    """Switch perspective of observation to other player."""
+    if env_name == "PongNoFrameskip-v4":
+        # TODO mirroring the observation shouldn't mirror the score!
+        return np.flip(obs, axis=-1).copy()
+    else:
+        raise ValueError("Unsupported environment for switching perspectives!")
 
 
-def get_hidden_activations(model, obs_a1, obs_a2):
-    """Given a pair of observations return activations for all layers."""
-    result, activation_a1 = get_extractor_activation(
-        model, obs_to_tensor(model, obs_a1)
-    )
-    result2, activation_a2 = get_extractor_activation(
-        model, obs_to_tensor(model, obs_a2)
-    )
-
-    return activation_a1, activation_a2
-
-
-def get_hidden_activations_dataset(model, dataset):
+def get_hidden_activations_dataset(env_name, model, dataset):
+    """Calculate hidden layer activations for given trajectory dataset."""
     hidden_act_dataset = []
     for traj in dataset:
         obs_1 = traj.obs
         th_obs_1 = obs_to_tensor(model, obs_1)
-        # TODO in pong mirroring the observation shouldn't mirror the score!
-        obs_2 = np.flip(obs_1, axis=-1).copy()
+        obs_2 = switch_player_perspective(env_name, obs_1)
         th_obs_2 = obs_to_tensor(model, obs_2)
         _, activations_1 = get_activations(
             model.policy.features_extractor, sb3_preprocess(model, th_obs_1)
@@ -89,7 +88,7 @@ def get_hidden_activations_dataset(model, dataset):
     return hidden_act_dataset
 
 
-class MLPProbe(nn.Module):
+class ValueProbe(nn.Module):
     def __init__(self, d):
         super().__init__()
         self.linear1 = nn.Linear(d, 1)
@@ -100,8 +99,11 @@ class MLPProbe(nn.Module):
 
 
 class CCS:
+    """Implementation of contrast consistent search for value functions."""
+
     def __init__(
         self,
+        env_name,
         model_name,
         num_epochs=1000,
         num_tries=10,
@@ -114,6 +116,8 @@ class CCS:
         val_fraction=0.2,
         seed=42,
     ):
+        self.env_name = env_name
+        self.model_name = model_name
         # training
         self.var_normalize = var_normalize
         self.num_epochs = num_epochs
@@ -130,15 +134,15 @@ class CCS:
         self.train_data, self.test_data = load_data(model_name, val_fraction, seed)
 
         self.train_activations = get_hidden_activations_dataset(
-            self.model, self.train_data
+            env_name, self.model, self.train_data
         )
         self.test_activations = get_hidden_activations_dataset(
-            self.model, self.test_data
+            env_name, self.model, self.test_data
         )
 
     def initialize_probe(self, layer_name):
         dim = self.train_activations[0][0][layer_name].shape[-1]
-        self.probe = MLPProbe(dim)
+        self.probe = ValueProbe(dim)
         self.probe.to(self.device)
 
     def normalize(self, activations):
@@ -153,47 +157,87 @@ class CCS:
         return normalized_x
 
     def get_loss(self, value_1, value_2):
-        """
-        Returns the CCS loss for two values each of shape (n,1) or (n,)
-        """
+        """Returns the CCS loss for two values each of shape (n,1) or (n,)."""
         # TODO add more loss options
         informative_loss = (torch.min((1 - value_1) ** 2, (1 - value_2) ** 2)).mean(0)
         consistent_loss = ((value_1 + value_2) ** 2).mean(0)
         return consistent_loss + informative_loss
 
-    def get_acc(self, x0_test, x1_test, y_test):
-        """
-        Computes accuracy for the current parameters on the given test inputs
-        """
-        # TODO normalize data in one call
-        x0 = torch.tensor(
-            self.normalize(x0_test),
-            dtype=torch.float,
-            requires_grad=False,
-            device=self.device,
+    def get_return_metrics(self):
+        """Computes metrics of value probe against trajectory returns."""
+        num_trajs = len(self.test_activations)
+        traj_lengths = [
+            len(self.test_activations[i][0][layer_name]) for i in range(num_trajs)
+        ]
+        x0 = (
+            torch.cat(
+                [self.test_activations[i][0][layer_name] for i in range(num_trajs)],
+                axis=0,
+            )
+            .detach()
+            .to(self.device)
         )
-        x1 = torch.tensor(
-            self.normalize(x1_test),
-            dtype=torch.float,
-            requires_grad=False,
-            device=self.device,
+        x1 = (
+            torch.cat(
+                [self.test_activations[i][1][layer_name] for i in range(num_trajs)],
+                axis=0,
+            )
+            .detach()
+            .to(self.device)
+        )
+        # compute returns from trajectory data assuming gamma=1
+        return_1 = (
+            torch.cat(
+                [
+                    torch.flip(
+                        torch.cumsum(
+                            torch.flip(torch.tensor(self.test_data[i].rews), dims=(0,)),
+                            dim=0,
+                        ),
+                        dims=(0,),
+                    )
+                    for i in range(num_trajs)
+                ],
+                axis=0,
+            )
+            .detach()
+            .to(self.device)
+        )
+        # TODO here we make the assumption that player 2 reward is the negative of player 1
+        return_2 = (
+            torch.cat(
+                [
+                    torch.flip(
+                        torch.cumsum(
+                            torch.flip(
+                                -torch.tensor(self.test_data[i].rews), dims=(0,)
+                            ),
+                            dim=0,
+                        ),
+                        dims=(0,),
+                    )
+                    for i in range(num_trajs)
+                ],
+                axis=0,
+            )
+            .detach()
+            .to(self.device)
         )
         with torch.no_grad():
             value_1, value_2 = self.best_probe(x0), self.best_probe(x1)
-        # TODO discuss whether we need this / can compute it
-        # - compare with value network if applicable
-        # - compare with expected returns from trajectories
-
-        # avg_confidence = 0.5*(p0 + (1-p1))
-        # predictions = (avg_confidence.detach().cpu().numpy() < 0.5).astype(int)[:, 0]
-        # acc = (predictions == y_test).mean()
-        # acc = max(acc, 1 - acc)
-        # return acc
+        avg_value_sum = (value_1 + value_2).mean()
+        avg_return_sum = (return_1 + return_2).mean()
+        value_1_loss = ((value_1 - return_1) ** 2).mean()
+        value_2_loss = ((value_2 - return_2) ** 2).mean()
+        return (
+            value_1_loss.cpu().item(),
+            value_2_loss.cpu().item(),
+            avg_value_sum.cpu().item(),
+            avg_return_sum.cpu().item(),
+        )
 
     def train(self, layer_name):
-        """
-        Does a single training run of nepochs epochs
-        """
+        """Train a single probe on the given hidden layer."""
         num_trajs = len(self.train_activations)
         x0 = (
             torch.cat(
@@ -216,15 +260,17 @@ class CCS:
 
         # set up optimizer
         optimizer = torch.optim.AdamW(
-            self.probe.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay
+            self.probe.parameters(),
+            lr=self.learning_rate,
+            weight_decay=self.weight_decay,
         )
 
         batch_size = len(x0) if self.batch_size == -1 else self.batch_size
-        nbatches = len(x0) // batch_size
+        num_batches = len(x0) // batch_size
 
         # Start training (full batch)
         for epoch in range(self.num_epochs):
-            for j in range(nbatches):
+            for j in range(num_batches):
                 x0_batch = x0[j * batch_size : (j + 1) * batch_size]
                 x1_batch = x1[j * batch_size : (j + 1) * batch_size]
 
@@ -242,6 +288,7 @@ class CCS:
         return loss.detach().cpu().item()
 
     def repeated_train(self, layer_name):
+        """Repeatedly train probes on given hidden layer."""
         best_loss = np.inf
         for train_num in range(self.num_tries):
             self.initialize_probe(layer_name)
@@ -251,7 +298,6 @@ class CCS:
                 print(f"New best loss!")
                 self.best_probe = copy.deepcopy(self.probe)
                 best_loss = loss
-
         return best_loss
 
 
@@ -263,21 +309,24 @@ if __name__ == "__main__":
     # Generate and save dataset if not exists
     data_save_path = f"datasets/{model_name}"
     if not os.path.isdir(data_save_path):
-        shutil.rmtree(data_save_path)
+        # shutil.rmtree(data_save_path)
         trajs = generate_dataset(
             env_name,
             model_name,
-            num_episodes=1,
-            max_episode_length=100,
+            num_episodes=10,
+            max_episode_length=500,
             num_envs=4,
             seed=42,
         )
         save(data_save_path, trajs)
 
     # Train multiple CCS probes on specified layer
-    ccs = CCS(model_name="ppo-PongNoFrameskip-v4")
+    ccs = CCS(env_name, model_name)
     ccs.repeated_train(layer_name)
 
-    # Evaluate
-    # ccs_acc = ccs.get_acc(neg_hs_test, pos_hs_test, y_test)
-    # print("CCS accuracy: {}".format(ccs_acc))
+    # Evaluate probe against trajectory returns
+    print(
+        "Best probe CCS eval metrics: v1_loss={:.5f}, v2_loss={:.5f}, avg_value_sum={:.5f}, avg_return_sum={:.5f}".format(
+            *ccs.get_return_metrics()
+        )
+    )
